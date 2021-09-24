c06f4b69581d233d4ac730a9fde1614725df5826
IgorSiejko <80519469+IgorSiejko@users.noreply.github.com>
2021-09-24 09:10:58 +0200

    Add files via upload

--- WinForm1/WinForm1/Form1.Designer.cs
+++ WinForm1/WinForm1/Form1.Designer.cs
@@ -35,24 +35,30 @@ namespace WinForm1
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TextBoxToInput = new System.Windows.Forms.TextBox();
+            this.TextBoxToInput2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBox11 = new System.Windows.Forms.TextBox();
             this.textBox12 = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.CopyButton1 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.ClearButton1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.ClearButton2 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(87, 154);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(42, 165);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 41);
+            this.button1.Size = new System.Drawing.Size(144, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "konwertuj";
             this.button1.UseVisualStyleBackColor = true;
@@ -60,7 +66,8 @@ namespace WinForm1
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(42, 48);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox1.Location = new System.Drawing.Point(42, 62);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(274, 41);
@@ -69,11 +76,11 @@ namespace WinForm1
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(521, 154);
+            this.button2.Location = new System.Drawing.Point(521, 165);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(121, 41);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Otwórz plik";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -83,43 +90,47 @@ namespace WinForm1
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(42, 252);
+            this.textBox2.Location = new System.Drawing.Point(42, 118);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(274, 41);
             this.textBox2.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 21);
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(105, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.Size = new System.Drawing.Size(155, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Input na base64";
             // 
-            // textBox3
+            // TextBoxToInput
             // 
-            this.textBox3.Location = new System.Drawing.Point(42, 309);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(274, 41);
-            this.textBox3.TabIndex = 5;
+            this.TextBoxToInput.Location = new System.Drawing.Point(42, 288);
+            this.TextBoxToInput.Multiline = true;
+            this.TextBoxToInput.Name = "TextBoxToInput";
+            this.TextBoxToInput.Size = new System.Drawing.Size(274, 41);
+            this.TextBoxToInput.TabIndex = 5;
             // 
-            // textBox4
+            // TextBoxToInput2
             // 
-            this.textBox4.Location = new System.Drawing.Point(42, 107);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(274, 41);
-            this.textBox4.TabIndex = 6;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.TextBoxToInput2.Location = new System.Drawing.Point(42, 350);
+            this.TextBoxToInput2.Multiline = true;
+            this.TextBoxToInput2.Name = "TextBoxToInput2";
+            this.TextBoxToInput2.ReadOnly = true;
+            this.TextBoxToInput2.Size = new System.Drawing.Size(274, 41);
+            this.TextBoxToInput2.TabIndex = 6;
+            this.TextBoxToInput2.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(87, 356);
+            this.button3.Location = new System.Drawing.Point(42, 397);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 41);
+            this.button3.Size = new System.Drawing.Size(144, 41);
             this.button3.TabIndex = 7;
             this.button3.Text = "konwertuj";
             this.button3.UseVisualStyleBackColor = true;
@@ -128,23 +139,15 @@ namespace WinForm1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(129, 225);
+            this.label2.Location = new System.Drawing.Point(130, 255);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 15);
             this.label2.TabIndex = 8;
             this.label2.Text = "base64 na input";
             // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(521, 48);
-            this.textBox9.Multiline = true;
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(274, 41);
-            this.textBox9.TabIndex = 13;
-            // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(521, 107);
+            this.textBox10.Location = new System.Drawing.Point(521, 119);
             this.textBox10.Multiline = true;
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(274, 41);
@@ -158,6 +161,7 @@ namespace WinForm1
             this.textBox11.Name = "textBox11";
             this.textBox11.Size = new System.Drawing.Size(274, 41);
             this.textBox11.TabIndex = 15;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
             // 
             // textBox12
             // 
@@ -189,13 +193,73 @@ namespace WinForm1
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(673, 154);
+            this.button4.Location = new System.Drawing.Point(673, 166);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(122, 41);
             this.button4.TabIndex = 22;
-            this.button4.Text = "Otwórz plik";
+            this.button4.Text = "Stwórz plik";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // CopyButton1
+            // 
+            this.CopyButton1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CopyButton1.Location = new System.Drawing.Point(192, 166);
+            this.CopyButton1.Name = "CopyButton1";
+            this.CopyButton1.Size = new System.Drawing.Size(49, 40);
+            this.CopyButton1.TabIndex = 23;
+            this.CopyButton1.Text = "kopiuj";
+            this.CopyButton1.UseVisualStyleBackColor = true;
+            this.CopyButton1.Click += new System.EventHandler(this.CopyButton1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(502, 239);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(13, 8);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // ClearButton1
+            // 
+            this.ClearButton1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ClearButton1.Location = new System.Drawing.Point(247, 166);
+            this.ClearButton1.Name = "ClearButton1";
+            this.ClearButton1.Size = new System.Drawing.Size(69, 40);
+            this.ClearButton1.TabIndex = 25;
+            this.ClearButton1.Text = "wyczyść";
+            this.ClearButton1.UseVisualStyleBackColor = true;
+            this.ClearButton1.Click += new System.EventHandler(this.CopyButton2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(192, 397);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 41);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "kopiuj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // ClearButton2
+            // 
+            this.ClearButton2.Location = new System.Drawing.Point(247, 397);
+            this.ClearButton2.Name = "ClearButton2";
+            this.ClearButton2.Size = new System.Drawing.Size(69, 41);
+            this.ClearButton2.TabIndex = 27;
+            this.ClearButton2.Text = "wyczyść";
+            this.ClearButton2.UseVisualStyleBackColor = true;
+            this.ClearButton2.Click += new System.EventHandler(this.ClearButton2_Click_1);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(521, 62);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(274, 41);
+            this.textBox9.TabIndex = 13;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
             // 
             // Form1
             // 
@@ -203,6 +267,11 @@ namespace WinForm1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.ClearButton2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.ClearButton1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.CopyButton1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button6);
@@ -212,8 +281,8 @@ namespace WinForm1
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.TextBoxToInput2);
+            this.Controls.Add(this.TextBoxToInput);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -221,7 +290,6 @@ namespace WinForm1
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -235,17 +303,22 @@ namespace WinForm1
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox TextBoxToInput;
+        private System.Windows.Forms.TextBox TextBoxToInput2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.TextBox textBox11;
         private System.Windows.Forms.TextBox textBox12;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button CopyButton1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button ClearButton1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button ClearButton2;
+        private System.Windows.Forms.TextBox textBox9;
     }
 }
 
--- WinForm1/WinForm1/Form1.cs
+++ WinForm1/WinForm1/Form1.cs
@@ -1,33 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinForm1
 {
     public partial class Form1 : Form
     {
-      
-
-       static public string EncodeTo64(string toEncode)
-
+        static public string EncodeTo64(string toEncode)
         {
-            byte[] toEncodeAsBytes
-
-                  = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
-
-            string returnValue
-
-                  = System.Convert.ToBase64String(toEncodeAsBytes);
-
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
             return returnValue;
-           
-
         }
         static string DecodeFrom64(string encodedData)
         {
@@ -36,68 +20,136 @@ namespace WinForm1
             string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
             return returnValue;
         }
-
         public Form1()
         {
             InitializeComponent();
         }
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-        }
+        //INPUT NA BASE 64-Sprawne
+        //
+        //
+                private void button1_Click(object sender, EventArgs e)
+                {
+                    static string EncodeTo64(string toEncode)
+                    {
+                        byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+                        string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+                        return returnValue;
+                    }
+                    string myData = textBox1.Text;
+                    string myDataEncoded = EncodeTo64(myData);
+                    if (String.IsNullOrEmpty(myData))
+                    {
+                        MessageBox.Show("Wypełnij Pole!");
+                    }
+                    else
+                    {
+                        textBox2.Text = myDataEncoded;
+                    }
+                }
+                private void CopyButton1_Click(object sender, EventArgs e)
+                {
+                    if (String.IsNullOrEmpty(textBox2.Text))
+                    {
+                        MessageBox.Show("Wypełnij pole, aby skopiować");
+                    }
+                    else
+                    {
+                        Clipboard.SetText(textBox2.Text);
+                    }
+                }
+                private void CopyButton2_Click(object sender, EventArgs e)
+                {
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+          //KONIEC INPUT NA 64
+         //
+        //
 
-        private void button1_Click(object sender, EventArgs e)
+
+          //BASE 64 NA INPUT-sprawne
+         //
+        //
+        private void button3_Click(object sender, EventArgs e)
         {
-            static  string EncodeTo64(string toEncode)
+            string myData = TextBoxToInput.Text;
+            string test = ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(myData));
+
+            //string myDataEncoded = EncodeTo64(myData);
+            //string myDataUnencoded = DecodeFrom64(myDataEncoded);
+            if (String.IsNullOrEmpty(myData))
             {
-                byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
-                string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
-                return returnValue;
+                MessageBox.Show("Wypełnij Pole!");
+            }
+            else
+            {
+                TextBoxToInput2.Text = test;
+            }
+        }
+        
+         private void button5_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(TextBoxToInput2.Text))
+            {
+                MessageBox.Show("Wypełnij pole, aby skopiować");
+            }
+            else
+            {
+                Clipboard.SetText(TextBoxToInput2.Text);
             }
 
-            string myData = "text";
-            string myDataEncoded = EncodeTo64(myData);
-            MessageBox.Show(myDataEncoded);
-      
         }
-         private void button3_Click(object sender, EventArgs e)
+        private void ClearButton2_Click_1(object sender, EventArgs e)
         {
-            static string DecodeFrom64(string encodedData)
+            TextBoxToInput.Clear();
+            TextBoxToInput2.Clear();
+        }
+          //BASE 64 NA INPUT
+         //
+        //
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\xopero\Desktop\test.txt";
+
+            
+
+         /*  if(File.Exists(path))
              {
-                 byte[] encodedDataAsBytes = System.Convert.FromBase64String(encodedData);
-                 string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
-                 return returnValue;
+                 MessageBox.Show("plik już istnieje");
              }
-
-             string myData = "VGVzdA==";
-             //string myData = "test"
-             string myDataEncoded = EncodeTo64(myData);
-             string myDataUnencoded = DecodeFrom64(myDataEncoded);
-
-             MessageBox.Show(myDataUnencoded);
+             else
+             {
+         */
+          
+                 // Open the file to read from.
+                 using (StreamReader sr = File.OpenText(path))
+                 {
+                     string s = "";
+                     while ((s = sr.ReadLine()) != null)
+                     {
+                         Console.WriteLine(s);
+                         MessageBox.Show(s);
+                     }
+                 }
+            
             
 
         }
 
-     
-    
-
-       
-
-        private void button2_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
+
         }
 
-            private void textBox1_TextChanged(object sender, EventArgs e)
-            {
-                
-            }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
 
-            private void textBox4_TextChanged(object sender, EventArgs e)
-            {
-
-            }
+        }
 
         private void textBox12_TextChanged(object sender, EventArgs e)
         {
@@ -116,14 +168,49 @@ namespace WinForm1
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //string text = System.IO.File.ReadAllText(@"C:\Users\xopero\Desktop\test.txt");
-            //MessageBox.Show(text);
-            string FileName = @"C: \Users\xopero\Desktop\test.txt";
-
+            //zmienna, która zawiera ścieżkę, w której zapisze się plik i jaki będzie miał format oraz nazwę
+            string path = @"C:\Users\xopero\Desktop\test.txt";
+            string text1 = textBox9.Text;
+            string text2 = textBox10.Text;
           
+            string info1="Wpisana Wartość: ";
+            string info2="  Zakodowana Wiadomość:";
+          
+                //Tworzy plik i nadaje mu treść
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(info1+text1+info1+text2);
+
+                }
+          
+            /*
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            */
+
+
+
 
         }
 
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
 }
 
