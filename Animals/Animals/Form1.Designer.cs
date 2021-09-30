a2cf69a14cc99a4b08d1d86ee806791b0fe7953a
IgorSiejko <80519469+IgorSiejko@users.noreply.github.com>
2021-09-30 15:25:36 +0200

    Add files via upload

--- Animals/Animals/Form1.Designer.cs
+++ Animals/Animals/Form1.Designer.cs
@@ -29,40 +29,25 @@ namespace Animals
         /// </summary>
         private void InitializeComponent()
         {
-            this.Hippo = new System.Windows.Forms.Label();
-            this.Wolf = new System.Windows.Forms.Label();
             this.Dog = new System.Windows.Forms.Label();
             this.Lion = new System.Windows.Forms.Label();
             this.Cat = new System.Windows.Forms.Label();
             this.Tiger = new System.Windows.Forms.Label();
+            this.Hippo1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Wolf = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // Hippo
-            // 
-            this.Hippo.AutoSize = true;
-            this.Hippo.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Hippo.Location = new System.Drawing.Point(68, 148);
-            this.Hippo.Name = "Hippo";
-            this.Hippo.Size = new System.Drawing.Size(40, 15);
-            this.Hippo.TabIndex = 0;
-            this.Hippo.Text = "Hippo";
-            // 
-            // Wolf
-            // 
-            this.Wolf.AutoSize = true;
-            this.Wolf.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Wolf.Location = new System.Drawing.Point(296, 148);
-            this.Wolf.Name = "Wolf";
-            this.Wolf.Size = new System.Drawing.Size(34, 15);
-            this.Wolf.TabIndex = 1;
-            this.Wolf.Tag = "label2";
-            this.Wolf.Text = "Wolf";
             // 
             // Dog
             // 
             this.Dog.AutoSize = true;
             this.Dog.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Dog.Location = new System.Drawing.Point(184, 148);
+            this.Dog.Location = new System.Drawing.Point(375, 43);
             this.Dog.Name = "Dog";
             this.Dog.Size = new System.Drawing.Size(30, 15);
             this.Dog.TabIndex = 2;
@@ -98,17 +83,97 @@ namespace Animals
             this.Tiger.TabIndex = 5;
             this.Tiger.Text = "Tiger";
             // 
+            // Hippo1
+            // 
+            this.Hippo1.Location = new System.Drawing.Point(48, 321);
+            this.Hippo1.Name = "Hippo1";
+            this.Hippo1.Size = new System.Drawing.Size(159, 47);
+            this.Hippo1.TabIndex = 6;
+            this.Hippo1.Text = "Pokaz informacje";
+            this.Hippo1.UseVisualStyleBackColor = true;
+            this.Hippo1.Click += new System.EventHandler(this.Hippo1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(296, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(414, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(535, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(650, 294);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Wolf
+            // 
+            this.Wolf.AutoSize = true;
+            this.Wolf.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Wolf.Location = new System.Drawing.Point(455, 82);
+            this.Wolf.Name = "Wolf";
+            this.Wolf.Size = new System.Drawing.Size(34, 15);
+            this.Wolf.TabIndex = 1;
+            this.Wolf.Tag = "label2";
+            this.Wolf.Text = "Wolf";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 98);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 205);
+            this.textBox1.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1262, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Hippo1);
             this.Controls.Add(this.Tiger);
             this.Controls.Add(this.Cat);
             this.Controls.Add(this.Lion);
             this.Controls.Add(this.Dog);
             this.Controls.Add(this.Wolf);
-            this.Controls.Add(this.Hippo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -118,12 +183,18 @@ namespace Animals
 
         #endregion
 
-        private System.Windows.Forms.Label Hippo;
-        private System.Windows.Forms.Label Wolf;
         private System.Windows.Forms.Label Dog;
         private System.Windows.Forms.Label Lion;
         private System.Windows.Forms.Label Cat;
         private System.Windows.Forms.Label Tiger;
+        private System.Windows.Forms.Button Hippo1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label Wolf;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Animals/Animals/Form1.cs
+++ Animals/Animals/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Animals
 {
     public partial class Form1 : Form
@@ -15,48 +16,78 @@ namespace Animals
         public Form1()
         {
             InitializeComponent();
-           
-
         }
         public class animal
         {
-            public void  Picture()
-                {
-                }
+            public void  Picture(string path)
+               {
+                //string path= @"E:\epsonScanner.png";
+               // Add(Image.FromFile(path));
+            }
+            /*
             public void Food()
             {
                 string plants = "trawa";
                 string meat = "mieso";
             }
-            public void Hunger(int hunger = 100) {
-              
-                int getHungry;
-                for(int i = 0;i<=100;i++)
+            */
+            public int hunger=100;
+            public void Hunger() {
+                //int getHungry;
+                /*
+                for(int i = 0;i<=100;i--)
                 {
                     getHungry = hunger - 1;
                     MessageBox.Show(getHungry.ToString());
                 }
+                */
             }
+            //
+            public int x;
+            public int y;
             public void Boundaries()
             {
-                int maxHeihgt = 100;
+                int maxHeight = 100;
                 int maxWidth = 100;
+                Console.WriteLine(maxHeight+"na"+maxWidth);
+                if(x>maxWidth && y>maxHeight)
+                {
+                    
+                    MessageBox.Show("Położenie zwierzaka jest nieprawidłowe, spróbuj ponownie");
+                }
+                else if(x < maxWidth && y > maxHeight) { MessageBox.Show("Położenie zwierzaka jest nieprawidłowe, spróbuj ponownie");}
+                else if (x > maxWidth && y < maxHeight) { MessageBox.Show("Położenie zwierzaka jest nieprawidłowe, spróbuj ponownie"); }
+                else
+                {
+                    MessageBox.Show("Dane są poprawne");
+                }
+
                 // rozmiar wybiegu
             }
             public void Locations(){
-                int x;
-                int y;
             }
-            public void MakeNoise(string noise)
+            //
+            public int chooseSound;
+            public void MakeNoise()
             {
-                string[] sounds = { "Hau Hau","Miau Miau","Roar Roar", "Hip Hip"};
+                string[] sounds = {"","Hau Hau","Miau Miau","Roar Roar","Hip Hip"};
+                /*
+                string[] sounds;
+                sounds[0] = "Hau Hau";
+                sounds[1] = "Miau Miau";
+                sounds[2] = "Roar Roar";
+                sounds[3] = "Hip Hip";
+                */
+                MessageBox.Show(sounds[chooseSound]);
             }
+
+            //
+            public bool food;
             public void Eat()
             {
                 //true = trawa
                 //false = mieso
-                bool food=true;
-                if(food == true)
+                if (food == true)
                 {
                     MessageBox.Show("Je trawke");
                 }
@@ -65,24 +96,52 @@ namespace Animals
                     MessageBox.Show("Je miesko");
                 }
             }
-            public void Sleep(){}
-            public void Roam(){}
-            public class Canine 
+            //
+
+            //
+            public bool isSleeping;
+            public int sleepTime;
+            public void Sleep()
             {
-                public class Hippo {}
-                public class Dog { }
-                public class Wolf { }
-                public class Lion { }
-                public class Cat { }
-                public class Tiger { }
-
-
+                if (isSleeping==true) 
+                {
+                    MessageBox.Show("Śpi");
+                }
+                else
+                {
+                    MessageBox.Show("Nie Śpi");
+                }
             }
-          
+            //
+            public void Roam(){}
+        }
+        public class Hippo : animal {}
+        private void Hippo1_Click(object sender, EventArgs e)
+        {
+            Hippo spot = new Hippo();
+            spot.food = false;
+            spot.isSleeping = false;
+            spot.sleepTime=10;
+            spot.x=160;
+            spot.y=100;
+            spot.chooseSound = 1;
+            spot.Eat();
+            spot.Hunger();
+            spot.Roam();
+            spot.Sleep();
+            spot.Roam();
+            spot.Locations();
+            spot.Boundaries();
+            spot.MakeNoise();
+           // MessageBox.Show();
         }
     }
+            /*
 
-
-
-        
-}
+            public class Dog { }
+            public class Wolf { }
+            public class Lion { }
+            public class Cat { }
+            public class Tiger { }
+            */
+        }
