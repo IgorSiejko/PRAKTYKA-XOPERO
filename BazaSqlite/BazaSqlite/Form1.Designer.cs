ccfa164f9c1ee38c45cc815d18373c04ef55fe02
IgorSiejko <80519469+IgorSiejko@users.noreply.github.com>
2021-10-07 15:54:19 +0200

    Add files via upload

---
+++ BazaSqlite/BazaSqlite/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace BazaSqlite
+{
+    partial class ConnSQLite
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnSql = new System.Windows.Forms.Button();
+            this.Name1 = new System.Windows.Forms.TextBox();
+            this.Lastname1 = new System.Windows.Forms.TextBox();
+            this.Adres1 = new System.Windows.Forms.TextBox();
+            this.Phonenmbr1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.Lastname = new System.Windows.Forms.Label();
+            this.Adres = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConnSql
+            // 
+            this.ConnSql.Location = new System.Drawing.Point(363, 390);
+            this.ConnSql.Name = "ConnSql";
+            this.ConnSql.Size = new System.Drawing.Size(75, 23);
+            this.ConnSql.TabIndex = 0;
+            this.ConnSql.Text = "button1";
+            this.ConnSql.UseVisualStyleBackColor = true;
+            this.ConnSql.Click += new System.EventHandler(this.ConnSql_Click_1);
+            // 
+            // Name1
+            // 
+            this.Name1.Location = new System.Drawing.Point(115, 37);
+            this.Name1.Name = "Name1";
+            this.Name1.Size = new System.Drawing.Size(100, 23);
+            this.Name1.TabIndex = 1;
+            // 
+            // Lastname1
+            // 
+            this.Lastname1.Location = new System.Drawing.Point(115, 92);
+            this.Lastname1.Name = "Lastname1";
+            this.Lastname1.Size = new System.Drawing.Size(100, 23);
+            this.Lastname1.TabIndex = 2;
+            // 
+            // Adres1
+            // 
+            this.Adres1.Location = new System.Drawing.Point(115, 151);
+            this.Adres1.Name = "Adres1";
+            this.Adres1.Size = new System.Drawing.Size(100, 23);
+            this.Adres1.TabIndex = 3;
+            // 
+            // Phonenmbr1
+            // 
+            this.Phonenmbr1.Location = new System.Drawing.Point(115, 208);
+            this.Phonenmbr1.Name = "Phonenmbr1";
+            this.Phonenmbr1.Size = new System.Drawing.Size(100, 23);
+            this.Phonenmbr1.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(338, 34);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(338, 92);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(533, 34);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 23);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(533, 92);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 23);
+            this.textBox8.TabIndex = 8;
+            // 
+            // Lastname
+            // 
+            this.Lastname.AutoSize = true;
+            this.Lastname.Location = new System.Drawing.Point(32, 95);
+            this.Lastname.Name = "Lastname";
+            this.Lastname.Size = new System.Drawing.Size(57, 15);
+            this.Lastname.TabIndex = 9;
+            this.Lastname.Text = "Nazwisko";
+            // 
+            // Adres
+            // 
+            this.Adres.AutoSize = true;
+            this.Adres.Location = new System.Drawing.Point(32, 151);
+            this.Adres.Name = "Adres";
+            this.Adres.Size = new System.Drawing.Size(37, 15);
+            this.Adres.TabIndex = 11;
+            this.Adres.Text = "Adres";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Number tel";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(281, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(338, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Imie";
+            // 
+            // ConnSQLite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Adres);
+            this.Controls.Add(this.Lastname);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Phonenmbr1);
+            this.Controls.Add(this.Adres1);
+            this.Controls.Add(this.Lastname1);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.ConnSql);
+            this.Name = "ConnSQLite";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnSql;
+        private System.Windows.Forms.TextBox Name1;
+        private System.Windows.Forms.TextBox Lastname1;
+        private System.Windows.Forms.TextBox Adres1;
+        private System.Windows.Forms.TextBox Phonenmbr1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label Lastname;
+      
+        private System.Windows.Forms.Label Adres;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BazaSqlite/BazaSqlite/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace BazaSqlite
+{
+    public partial class ConnSQLite : Form
+    {
+        public ConnSQLite()
+        {
+            InitializeComponent();
+        }
+       
+            public void connection()
+            {
+                string cs = @"URI=file:C:\Users\xopero\source\repos\BazaSqlite\BazaSqlite\test.db";
+                using var con = new SQLiteConnection(cs);
+                con.Open();
+            using var crt = new SQLiteCommand(con);
+
+            crt.CommandText =
+                @"CREATE TABLE Person
+            (
+                ID   INTEGER,
+                Name  TEXT,
+                LastName  TEXT,
+                Email TEXT UNIQUE,
+                PhoneNumber   NUMERIC UNIQUE,
+                PRIMARY KEY(ID AUTOINCREMENT)
+            );";
+                crt.ExecuteNonQuery();
+
+
+                crt.CommandText =
+                @"CREATE TABLE Company
+            (
+                ID  INTEGER UNIQUE,
+                Nazwa   TEXT UNIQUE,
+                Adres   TEXT,
+                NIP INTEGER UNIQUE,
+                REGON   INTEGER UNIQUE,
+                PRIMARY KEY(ID AUTOINCREMENT)
+            );";
+                crt.ExecuteNonQuery();
+
+                crt.CommandText =
+                @"CREATE TABLE OrderData 
+            (
+                ID INTEGER UNIQUE,
+	            Product TEXT,
+                NaKiedy TEXT,
+	            Adres TEXT,
+                Cost    NUMERIC,
+	            PRIMARY KEY(ID AUTOINCREMENT)
+            );";
+                crt.ExecuteNonQuery();
+
+            crt.CommandText = "INSERT INTO Person(Name, LastName, Email, PhoneNumber) VALUES('@dwa','@trzu','@cztery','@piec')";
+            crt.ExecuteNonQuery();
+            crt.CommandText = "INSERT INTO Company(Nazwa, Adres, NIP, REGON) VALUES('@dwa','@trzu','@cztery','@piec')";
+            crt.ExecuteNonQuery();
+            crt.CommandText = "INSERT INTO OrderData(Product, NaKiedy, Adres, Cost) VALUES('@dwa', '@trzu', '@cztery', '@piec')";
+            crt.ExecuteNonQuery();
+            /*            
+            crt.CommandText = "DROP TABLE IF EXISTS OrderData";
+            crt.ExecuteNonQuery();
+            crt.CommandText = "DROP TABLE IF EXISTS Company";
+            crt.ExecuteNonQuery();
+            crt.CommandText = "DROP TABLE IF EXISTS Person";
+            crt.ExecuteNonQuery();*/
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //connection();
+        }
+        private void ConnSql_Click_1(object sender, EventArgs e)
+        {
+        }
+        static void ConnSql_Click(object sender, EventArgs e)
+            {
+                /* SQLiteConnection sqlite_conn;
+                 SQLiteConnection sqlite_cmd;
+                 SQLiteDataReader sqlite_datereader;
+                */
+                //crt.CommandText = "INSERT INTO Person(Name, LastName, Email, PhoneNumber) VALUES()";
+         
+            }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+}
